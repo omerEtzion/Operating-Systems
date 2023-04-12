db80dc0c2bccddc976cf13c0ebfbe9f8076babbd
omerEtzion <etzion.omer@gmail.com>
2023-04-12 16:57:12 +0300

    submission

--- xv6-riscv/kernel/main.c
+++ xv6-riscv/kernel/main.c
@@ -16,35 +16,20 @@ main()
     printf("\n");
     printf("xv6 kernel is booting\n");
     printf("\n");
-    // printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
     kinit();         // physical page allocator
-    // printf("after kinit\n");
     kvminit();       // create kernel page table
-    // printf("after kvminit\n");
     kvminithart();   // turn on paging
-    // printf("after kvminithart\n");
     procinit();      // process table
-    // printf("after procinit\n");
     trapinit();      // trap vectors
-    // printf("after trapinit\n");
     trapinithart();  // install kernel trap vector
-    // printf("after trapinithart\n");
     plicinit();      // set up interrupt controller
-    // printf("after plicinit\n");
     plicinithart();  // ask PLIC for device interrupts
-    // printf("after plicinithart\n");
     binit();         // buffer cache
-    // printf("after binit\n");
     iinit();         // inode table
-    // printf("after iinit\n");
     fileinit();      // file table
-    // printf("after fileinit\n");
     virtio_disk_init(); // emulated hard disk
-    // printf("after virtio_disk_init\n");
     userinit();      // first user process
-    // printf("after userinit\n");
     __sync_synchronize();
-    // printf("after __sync_synchronize\n");
     started = 1;
   } else {
     while(started == 0)
